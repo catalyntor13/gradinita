686471732006573d3a9be28b85d0477ef70d3c88
catalyntor13 <catalintor19@gmail.com>
2025-10-08 13:35:13 +0300

    Fixed some SEO keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,23 @@ export const metadata: Metadata = {
   title: 'Gradinita Busy Bee Ramnicu Valcea',
   description: "Oferim un mediu sigur, cald și stimulativ pentru dezvoltarea armonioasă a copilului tău. Cu educatori dedicați și programe inovatoare.",
   keywords: ["Gradinita Valcea", "Gradinita Busy Bee", "Gradinita Busy Bee Valcea", "Gradinita Rm Valcea", "Gradinita Busy Bee Ramnicu Valcea", "Cresa Valcea", "Gradinita acreditata Valcea", "Gradinita privata", "Gradinita Ramnicu Valcea"],
-  
+  authors: [{ name: "Gradinita Busy Bee" }],
+  openGraph: {
+    title: "Gradinita Busy Bee – Învățăm prin joacă în Ramnicu Valcea",
+    description: "Oferim un mediu sigur, cald și stimulativ pentru dezvoltarea armonioasă a copilului tău. Cu educatori dedicați și programe inovatoare.",
+    url: "https://www.gradinitabusybee.ro/",
+    siteName: "Gradinita Busy Bee",
+    images: [
+      {
+        url: "https://www.gradinitabusybee.ro/logo.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Copii la Gradinita Busy Bee"
+      }
+    ],
+    locale: "ro_RO",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
